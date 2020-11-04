3d37e99dd96a1db60710f9f569c0e5bb5a1d263a
Finn Stoldt <stoldtf@gmail.com>
2020-11-04 17:51:29 +0100

    Updated vCard file name

--- backend/src/controllers/contactController.js
+++ backend/src/controllers/contactController.js
@@ -1,10 +1,17 @@
 const r = require('express').Router();
+const { config } = require('../config');
 const { vCard } = require('../util/vCard');
 
 r.get('/vcard', (req, res) => {
   // set content-type and disposition including desired filename
-  res.set('Content-Type', 'text/vcard; name="contact.vcf"');
-  res.set('Content-Disposition', 'inline; filename="contact.vcf"');
+  res.set(
+    'Content-Type',
+    `text/vcard; name="${config.contact.firstName}${config.contact.lastName}.vcf"`,
+  );
+  res.set(
+    'Content-Disposition',
+    `inline; filename="${config.contact.firstName}${config.contact.lastName}.vcf"`,
+  );
 
   // send the response
   return res.send(vCard.getFormattedString());
